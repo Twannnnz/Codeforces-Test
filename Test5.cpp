e0c905aa4a566985c92fe370af9de5819b0c69dc
Twannnnz <huyvip292@gmail.com>
2025-10-21 19:40:42 +0700

    Update Test5.cpp

--- Test5.cpp
+++ Test5.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    int n , m;
+    bool found = true; 
+    for ( int i  = 0 ; i < s1.length() && found  ; i++ ){
+        for ( int j = 0 ; j < s2.length() ; j++){
+            if (s1[i] == s2[j]){
+                n = i;
+                m = j;
+                found = false;
+                break;
+            }
+        }
+    }
+
+
+    for ( int i = 0 ; i < s2.length() ; i++ ){
+        for ( int j = 0 ; j < s1.length() ; j++ ){
+            if ( i == m ){
+                cout << s1[j];
+            } 
+            else if ( j == n ){
+                cout << s2[i];
+            }
+            else {
+                cout << ".";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
 
